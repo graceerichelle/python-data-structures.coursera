e97e829f9fa9139c7438b2a5210fdc59e1d3b141
graceerichelle <67563520+graceerichelle@users.noreply.github.com>
2020-07-19 19:43:48 -0400

    Create week-four-8.4assignment.py

---
+++ week-four-8.4assignment.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+collection = fh.read()
+wordlist = list()
+
+for wordloop in collection.split():
+    value = wordloop
+    if value in wordlist:
+        continue
+    wordlist.append(value)
+
+wordlist.sort()
+print(wordlist)
